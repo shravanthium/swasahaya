d09b866b1640ed478cb339eff2c773ff0b835270
shravanthium <shravanthi@wwstay.com>
2019-05-05 21:46:03 +0530

    Renamed app name and  new bank app

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn gettingstarted.wsgi --log-file -
+web: gunicorn swasahaya.wsgi --log-file -
---
+++ bank/models.py
@@ -0,0 +1,20 @@
+# core django
+from django.db import models
+
+# third party
+from model_utils.models import TimeStampedModel
+
+
+class Bank(TimeStampedModel):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    bank_name = models.CharField(max_length=180)
+    address = models.CharField(max_length=256)
+    city = models.CharField(max_length=50)
+    district = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    branch = models.CharField(max_length=256)
+    ifsc = models.CharField(max_length=15)
+    bank_id = models.IntegerField()
+    
+    def __str__(self):
+        return self.bank_name
