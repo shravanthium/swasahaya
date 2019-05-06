1e34f95d44251ee99ecc6436dc63e52bed86ecfc
shravanthium <shravanthi@wwstay.com>
2019-05-06 10:56:09 +0530

    Order the urls

--- swasahaya/urls.py
+++ swasahaya/urls.py
@@ -11,8 +11,8 @@ bank_list = bank.api.BankViewSet.as_view({'get': 'list'})
 bank_detail = bank.api.BankViewSet.as_view({'get': 'retrieve'})
 
 urlpatterns = [
-    re_path(r'^banks/(?P<ifsc_code>\w+)/$', bank_detail, name='bank_detail'),
     path('', bank.views.HomePageView.as_view(), name='index'),
     path('banks/', bank_list, name='bank_list'),
+    re_path(r'^banks/(?P<ifsc_code>\w+)/$', bank_detail, name='bank_detail'),
     path("admin/", admin.site.urls),
 ]
