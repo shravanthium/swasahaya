35fba48cfedfbf9c70028fa18f5b9e140baac876
shravanthium <shravanthi@wwstay.com>
2019-05-06 10:55:18 +0530

    List all banks in home page

--- bank/views.py
+++ bank/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views import generic
 
-# Create your views here.
-class HomePageView(TemplateView):
-    def get(self, request, **kwargs):
-        return render(request, 'index.html', context=None)
+from bank.models import *
+
+class HomePageView(generic.ListView):
+    model = Bank
+    template_name = 'index.html'
+    context_object_name = 'banks'
+
+    def get_queryset(self):
+        return Bank.objects.all()[:15]
